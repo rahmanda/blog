c991fe83398c657f0c5fab6599dc78dea4865692
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2024-05-20 22:42:32 +0200

    feat: setup blog homepage

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,14 +4,13 @@ const articlesCollection = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
-    summary: z.string(),
-    slug: z.string(),
-    published_date: z.string(),
-    language: z.enum(["id", "en"]),
+    summary: z.string().optional(),
+    published_date: z.date(),
+    language: z.enum(["id", "en"]).optional(),
   }),
 });
 
 export const collections = {
   blog: articlesCollection,
-  til: articlesCollection,
+  notes: articlesCollection,
 };
