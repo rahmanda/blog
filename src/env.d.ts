9958992b360c072e890c5d1b1ed49cbf2037daf8
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2024-05-23 16:49:30 +0200

    fix: resolve global typing

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,12 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
-declare global {
-  interface Window {
-    _goodshare?: {
-      reNewAllInstance: () => void;
-      setShareCallback: (fn: () => void) => void;
-    };
-    dataLayer: any[];
-  }
+interface Window {
+  _goodshare?: {
+    reNewAllInstance: () => void;
+    setShareCallback: (fn: () => void) => void;
+  };
+  dataLayer: any[];
 }
